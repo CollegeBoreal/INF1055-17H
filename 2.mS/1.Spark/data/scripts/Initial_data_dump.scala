0d76819c6d3f605f050a3cc7255707dc9caff725
setrar <setrar@valiha.com>
2017-02-22 16:58:46 -0500

    Update Initial_data_dump.scala

--- 2.mS/1.Spark/data/scripts/Initial_data_dump.scala
+++ 2.mS/1.Spark/data/scripts/Initial_data_dump.scala
@@ -26,8 +26,8 @@ val urlDest = s"jdbc:mysql://db:3306/$nameDestDB?useSSL=false"
 // Importing film
 
 val df_films_oldDB = sqlContext.read.format("jdbc").option("url", urlSource).option("driver", driver).option("dbtable", "film").option("user", userSrcDB).option("password", passSrcDB).option("verifyServerCertificate", "false").load()
-val df_films_newDB = df_iso_countries_oldDB.select($"film_in_stock", $"film_not_in_stock")
-df_films_newDB.write.mode("append").jdbc(urlDest,"FILM",prop) // Overwrite existing countries
+val df_films_newDB = df_films_oldDB.select($"film_in_stock", $"film_not_in_stock")
+df_films_newDB.write.mode("append").jdbc(urlDest,"FILM",prop) // Overwrite existing film
 
 
 System.exit(0)
